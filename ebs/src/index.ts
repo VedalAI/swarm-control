512d9eb0a245eca92d4842256cca0c3a23a538ca
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2024-06-07 00:50:05 +0200

    Update index.ts

--- ebs/src/index.ts
+++ ebs/src/index.ts
@@ -7,11 +7,15 @@ import {privateApiAuth, publicApiAuth} from "./middleware";
 dotenv();
 
 export const app = express();
-app.use(cors({origin: "*"}))
+app.use(cors({origin: "ext-twitch.tv"}));
 app.use(bodyParser.json());
 app.use("/public/*", publicApiAuth);
 app.use("/private/*", privateApiAuth);
 app.listen(3000, () => console.log("Listening on port 3000"));
+
+app.get("/", (_, res) => {
+    res.send("YOU ARE TRESPASSING ON PRIVATE PROPERTY YOU HAVE 5 SECONDS TO GET OUT OR I WILL CALL THE POLICE");
+})
 
 require("./modules/config");
 require("./modules/transactions");
