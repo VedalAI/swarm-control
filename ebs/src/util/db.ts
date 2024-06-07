43140fd05f01fc180231899f207316483dd02b98
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2024-06-07 19:37:08 +0200

    Update db.ts

--- ebs/src/util/db.ts
+++ ebs/src/util/db.ts
@@ -58,11 +58,9 @@ export async function registerPrepurchase(cart: IdentifiableCart): Promise<strin
 
 export async function getPrepurchase(token: string): Promise<IdentifiableCart | undefined> {
     try {
-        const [rows] = (await db.query("SELECT cart FROM prepurchases WHERE token = ?", [token])) as [
-            RowDataPacket[],
-            any,
-        ];
-        console.log(rows);
+        const x = (await db.query("SELECT cart FROM prepurchases WHERE token = ?", [token])) as [RowDataPacket[], any];
+        console.log(x);
+        const rows = x[0];
         if (rows.length === 0) return undefined;
         return JSON.parse(rows[0].cart) as IdentifiableCart;
     } catch (e: any) {
